b71273bbc28ab2ebb5ee9f8cb46be4b42ebc4cee
isadev97 <isa325697@gmail.com>
2023-05-06 20:24:44 +0530

    sign up sign in views

--- media_app/urls.py
+++ media_app/urls.py
@@ -4,5 +4,6 @@ from media_app import views
 urlpatterns = [
     path('signup/', views.sign_up, name='sign_up' ),
     path('signin/', views.sign_in, name='sign_in'),
-    path('signout/', views.sign_out, name='sign_out')
+    path('signout/', views.sign_out, name='sign_out'),
+    path('profile-settings/', views.profile_settings, name='profile_settings')
 ]
--- media_app/views.py
+++ media_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import auth
 from media_app.models import User
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@csrf_exempt
 def sign_up(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -23,26 +26,45 @@ def sign_up(request):
         if user:
             # User is authenticated
             auth.login(request, user)
-            print("user created successfully", user)
+            return JsonResponse({"message" : "sign up"})
         else:
             print("some error is there")
     else:
         print("return sign up html page")
-
+        
+@csrf_exempt
 def sign_in(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
         user = auth.authenticate(username=username, password=password)
         if user:
+            # binding
             auth.login(request, user)
-            print("user is logged in", user)
+            return JsonResponse({"message" : "sign in"})
+            
         else:
             print("Some error is there")
     else:
         print("get method for sign in")
         print("render sign in  html page")
-        
+
+@csrf_exempt
 def sign_out(request):
+     # opposite of binding
     auth.logout(request)
-    print("logout is done")
+    return JsonResponse({"message" : "sign out"})
+    
+@login_required(login_url='sign_in')
+def profile_settings(request):
+    try:
+        response_data = {
+            "message" : "Hello, you are at profile settings page with current details",
+            "id": request.user.id,
+            "email": request.user.email,
+            "username": request.user.username
+        }
+        return JsonResponse(response_data)
+    except:
+        return JsonResponse({"message" : "you are already signed out"})
+        
