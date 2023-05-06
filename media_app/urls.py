4057b7d545b26a4ef9a63fcff3ebc3b50043439f
isadev97 <isa325697@gmail.com>
2023-05-06 19:52:47 +0530

    basic sign up sign in sign out views

--- media_app/urls.py
+++ media_app/urls.py
@@ -3,5 +3,6 @@ from media_app import views
 
 urlpatterns = [
     path('signup/', views.sign_up, name='sign_up' ),
-    path('signin/', views.sign_in, name='sign_in' )
+    path('signin/', views.sign_in, name='sign_in'),
+    path('signout/', views.sign_out, name='sign_out')
 ]
--- media_app/views.py
+++ media_app/views.py
@@ -3,6 +3,32 @@ from django.contrib.auth.models import auth
 from media_app.models import User
 
 # Create your views here.
+
+def sign_up(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            print("username already taken")
+            return redirect('signup')
+        
+        if User.objects.filter(email=email).exists():
+            print("email already taken")
+            return redirect('signup')
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            # User is authenticated
+            auth.login(request, user)
+            print("user created successfully", user)
+        else:
+            print("some error is there")
+    else:
+        print("return sign up html page")
+
 def sign_in(request):
     if request.method == "POST":
         username = request.POST["username"]
@@ -15,3 +41,8 @@ def sign_in(request):
             print("Some error is there")
     else:
         print("get method for sign in")
+        print("render sign in  html page")
+        
+def sign_out(request):
+    auth.logout(request)
+    print("logout is done")
