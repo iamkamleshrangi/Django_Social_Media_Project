475e8fd00b47dd473532b6d6771257c750487846
isadev97 <isa325697@gmail.com>
2023-05-06 19:35:12 +0530

    sign in view added

--- media_app/views.py
+++ media_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth
+from media_app.models import User
 
 # Create your views here.
+def sign_in(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            print("user is logged in", user)
+        else:
+            print("Some error is there")
+    else:
+        print("get method for sign in")
